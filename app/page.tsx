b00595b4348d41ed737096958af3ef6c99d2e720
mihiryadav20 <mihir.yadav@somaiya.edu>
2025-08-11 17:43:46 +0530

    changing the name

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
     <div className="relative min-h-screen w-full bg-neutral-950 flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
-          Welcome to Generalux
+          Welcome to Achileads
         </h1>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
         Unleash your market potential with our AI-powered GTM strategy tool.
